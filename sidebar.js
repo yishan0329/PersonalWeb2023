93f97f40282569d53a7d3857ef71ad19feaf346b
DESKTOP-LJ79RE2\user <10926104@gsuite.ntua.edu.tw>
2026-06-06 13:39:49 +0800

    20260606 增加DIGITIMES2023-index.html索引頁 整理sidebar

---
+++ sidebar.js
@@ -0,0 +1,10 @@
+// Sidebar open / close
+function Open() {
+    var sb = document.getElementById("mySidebar");
+    sb.style.width   = "100%";
+    sb.style.display = "block";
+}
+
+function Close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
